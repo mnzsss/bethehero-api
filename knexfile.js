0d7bf11ec904ccc0ecf00a1b7ca28401db6c7eaf
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-03-26 18:33:51 -0300

    Completed.

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,46 @@
 // Update with your config settings.
 
-export const development = {
-  client: 'sqlite3',
-  connection: {
-    filename: './dev.sqlite3',
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
   },
-};
-export const staging = {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password',
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
-};
-export const production = {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password',
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 };
--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -1,6 +1,34 @@
+import crypto from 'crypto';
+import connection from '../database';
+
 class OngController {
   async index(req, res) {
-    return res.json();
+    const ongs = await connection('ongs').select('*');
+
+    return res.json(ongs);
+  }
+
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const [ongExists] = await connection('ongs').where('name', name);
+
+    if (ongExists) {
+      return res.status(400).json({ error: 'Ong already exists.' });
+    }
+
+    const id = crypto.randomBytes(4).toString('hex');
+
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    });
+
+    return res.json({ id });
   }
 }
 
